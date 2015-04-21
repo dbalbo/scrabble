044e954d373cb92379e142c61730f6a684406c6d
Michael Ryan Yanchar & Debbie Balbo <student@epicodus.com>
2015-04-21 16:10:42 -0700

    successful assignment of values to letters

--- lib/scrabble.rb
+++ lib/scrabble.rb
@@ -1,14 +1,16 @@
 class String
   define_method(:scrabble) do
-    score_one = { "a"=> 1, "e"=> 1, "i"=> 1, "o"=> 1, "u"=> 1, "l"=> 1, "n"=> 1, "r"=> 1, "s"=> 1, "t"=> 1,}
-    score_two = { "d"=> 2, "g"=> 2}
-    score_three = { "b"=> 3, "c"=> 3, "m"=> 3, "p"=> 3}
-    score_four = { "f"=> 4, "h"=> 4, "y"=> 4, "v"=> 4, "w"=> 4}
-    score_five = { "k"=> 5}
-    score_eight = { "j"=> 8, "x"=> 8}
-    score_ten = { "q"=> 10, "z"=> 10}
+    score = { "a"=> 1, "e"=> 1, "i"=> 1, "o"=> 1, "u"=> 1, "l"=> 1, "n"=> 1, "r"=> 1, "s"=> 1, "t"=> 1,"d"=> 2, "g"=> 2,
+      "b"=> 3, "c"=> 3, "m"=> 3, "p"=> 3, "f"=> 4, "h"=> 4, "y"=> 4, "v"=> 4, "w"=> 4, "k"=> 5, "j"=> 8, "x"=> 8,
+      "q"=> 10, "z"=> 10}
+    total = 0
+    input = self
+    letters = input.split(//)
+    letters.each() do |letter|
+      total = total + score.fetch(letter)
 
-      input = self
-      values = input.split(//)
+
+    end
+    total
   end
 end
--- spec/scrabble_spec.rb
+++ spec/scrabble_spec.rb
@@ -1,15 +1,21 @@
 require('rspec')
 require('scrabble')
-
+require('pry')
 
 describe('Hash#scrabble') do
   it('returns a scrabble score for a letter') do
-    expect('a'.scrabble()).to(eq(10))
+    expect('a'.scrabble()).to(eq(1))
   end
 end
 
 describe('Hash#scrabble') do
   it('seperates a string into an array') do
-    expect('orange'.scrabble()).to(eq(["a", "p", "p", "l", "e"]))
+    expect('apple'.scrabble()).to(eq(9))
+  end
+end
+
+describe('Hash#scrabble') do
+  it('assigns a value for each string in the array') do
+    expect('add'.scrabble()).to(eq(5))
   end
 end
