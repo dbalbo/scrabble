aa3eeff436f45d9d60d8ba35f8b2fb85dea16224
Michael Ryan Yanchar & Debbie Balbo <student@epicodus.com>
2015-04-21 14:17:37 -0700

    it created all letter values equal to 1

--- lib/scrabble.rb
+++ lib/scrabble.rb
@@ -1,7 +1,6 @@
 class String
   define_method(:scrabble) do
-    score = Hash.new()
-    score.store("a", 1)
-      score.fetch("a")
+    score_one = { "a"=> 1, "e"=> 1, "i"=> 1, "o"=> 1, "u"=> 1, "l"=> 1, "n"=> 1, "r"=> 1, "s"=> 1, "t"=> 1,}
+      score_one.fetch("n")
   end
 end
